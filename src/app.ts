1a6eacf00e05ddfd1518d95c4c43c97e13786454
BunkerBusterDev <bunkerbusterdev@gmail.com>
2023-09-12 17:44:09 +0900

    cluster setting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
+import cluster from 'cluster';
+import os from 'os';
+
 import koa from 'koa';
 import morgan from 'koa-morgan';
+
 import { httpLogStream } from 'lib/winston';
 
 const processEnv: { [key: string]: string } = {};
@@ -9,23 +13,34 @@ dotenv.config({ processEnv });
 const { PORT: port } = processEnv;
 
 const app = new koa();
-
-const logger = global.getLogger('app');
-
-logger.error('error');
-logger.warn('warn');
-logger.info('info');
-logger.debug('debug');
-logger.silly('silly');
-
 const loggerStream = global.getLogger('httpLogStream');
-
 app.use(morgan('combined', { stream: httpLogStream(loggerStream) }));
 
 declare global {
     function test(): void;
 }
 
-app.listen(port, () => {
-    logger.info('Ready to start Server, listening port 38800');
-});
+const logger = global.getLogger('app');
+
+const cpuCount = os.cpus().length;
+const worker = [];
+const useClustering = true;
+
+if (useClustering) {
+    if (cluster.isPrimary) {
+        cluster.on('death', (worker) => {
+            console.log(`worker${worker.pid} died --> start again`);
+        });
+
+        logger.info(`CPU Count = ${cpuCount}`);
+        for (let i = 0; i < cpuCount; i++) {
+            worker[i] = cluster.fork();
+        }
+    } else {
+        app.listen(port, () => {
+            logger.info(`Worker${cluster.worker?.id} Ready to start Server, listening port ${port}`);
+        });
+    }
+} else {
+    logger.info(`Primary is working`);
+}
