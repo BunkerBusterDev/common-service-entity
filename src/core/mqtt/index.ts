3b420ef3deb412d8534b38c7526451e36b53ee40
BunkerBusterDev <bunkerbusterdev@gmail.com>
2023-09-27 11:56:54 +0900

    test

--- src/core/mqtt/index.ts
+++ src/core/mqtt/index.ts
@@ -20,7 +20,6 @@ export default class mqttServer {
         // this.cacheTTL = 3; // count
         this.port = config.proxyInfo.useProxyMqttPort === undefined ? '1883' : config.proxyInfo.useProxyMqttPort;
         this.mqttApp = new koa();
-        console.log('test');
         // this.httpResponseQueue = {};
         // this.messageCache = {};
         // this.responseMqttRequestIdentifieriArray = [];
